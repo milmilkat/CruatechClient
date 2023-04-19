78ad1754a802ff4ea67d4cf64413fc399034db6f
Milad Katebi <mldktb@gmail.com>
2023-04-19 16:45:27 +0200

    Add project files.

---
+++ CruatechClient/MainForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace CruatechClient
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConnectBtn = new System.Windows.Forms.Button();
+            this.ResultRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.DisconnectBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ConnectBtn
+            // 
+            this.ConnectBtn.Location = new System.Drawing.Point(48, 38);
+            this.ConnectBtn.Name = "ConnectBtn";
+            this.ConnectBtn.Size = new System.Drawing.Size(174, 64);
+            this.ConnectBtn.TabIndex = 0;
+            this.ConnectBtn.Text = "Connect";
+            this.ConnectBtn.UseVisualStyleBackColor = true;
+            this.ConnectBtn.Click += new System.EventHandler(this.ConnectBtn_Click);
+            // 
+            // ResultRichTextBox
+            // 
+            this.ResultRichTextBox.Location = new System.Drawing.Point(48, 129);
+            this.ResultRichTextBox.Name = "ResultRichTextBox";
+            this.ResultRichTextBox.Size = new System.Drawing.Size(696, 252);
+            this.ResultRichTextBox.TabIndex = 1;
+            this.ResultRichTextBox.Text = "";
+            // 
+            // DisconnectBtn
+            // 
+            this.DisconnectBtn.Location = new System.Drawing.Point(570, 38);
+            this.DisconnectBtn.Name = "DisconnectBtn";
+            this.DisconnectBtn.Size = new System.Drawing.Size(174, 64);
+            this.DisconnectBtn.TabIndex = 2;
+            this.DisconnectBtn.Text = "Diconnect";
+            this.DisconnectBtn.UseVisualStyleBackColor = true;
+            this.DisconnectBtn.Click += new System.EventHandler(this.DisconnectBtn_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DisconnectBtn);
+            this.Controls.Add(this.ResultRichTextBox);
+            this.Controls.Add(this.ConnectBtn);
+            this.Name = "MainForm";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ConnectBtn;
+        private System.Windows.Forms.RichTextBox ResultRichTextBox;
+        private System.Windows.Forms.Button DisconnectBtn;
+    }
+}
+
---
+++ CruatechClient/MainForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CruatechClient
+{
+    public partial class MainForm : Form
+    {
+        private bool _connected = false;
+        private Socket _client;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void ConnectBtn_Click(object sender, EventArgs e)
+        {
+            _client = new Socket(
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            await _client.ConnectAsync("localhost", 11111);
+            _connected = true;
+            while (_connected)
+            {
+                byte[] byteBuffer = new byte[256];
+                ArraySegment<byte> buffer = new ArraySegment<byte>(byteBuffer);
+
+                _ = await _client.ReceiveAsync(buffer, SocketFlags.None);
+                var index = 0;
+                for (var i = 0; i < 256; i++)
+                    if (buffer.Array[i] == 0x01)
+                        index = i;
+
+                var initialOffset = index + 2;
+
+                var length = Convert.ToInt16(buffer.Array[index + 1]);
+                var checkSum = Convert.ToInt16(buffer.Array[length + initialOffset]);
+                var response = Encoding.UTF8.GetString(buffer.Array, initialOffset, length);
+
+                var bufferValues = 0;
+
+                for (var i = initialOffset - 2; i < length + initialOffset; i++)
+                    bufferValues += buffer.Array[i];
+
+                var realChecksum = bufferValues & 0xFF;
+                if (realChecksum != checkSum)
+                    throw new Exception("Data is corrupted");
+
+                ResultRichTextBox.Text = response;
+            }
+
+            _client.Shutdown(SocketShutdown.Both);
+        }
+
+        private void DisconnectBtn_Click(object sender, EventArgs e)
+        {
+            _connected = false;
+            _client.Shutdown(SocketShutdown.Both);
+        }
+    }
+}
